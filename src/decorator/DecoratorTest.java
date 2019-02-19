4b7022b67869ca14b7be462f2e4ca3e371b68d38
lillusory <xueduanli@163.com>
2019-02-19 22:36:17 +0800

    fix Decorator Test

--- src/decorator/DecoratorTest.java
+++ src/decorator/DecoratorTest.java
@@ -10,14 +10,23 @@ import org.junit.jupiter.api.Test;
 public class DecoratorTest {
     @Test
     public void decoratorTest() {
+        //普通对象
         Human man = new Man();
         man.run();
         System.out.println("--------------------");
+        //装饰后的对象
         Human flyMan = new FlyMan(man);
         flyMan.run();
         System.out.println("--------------------");
-        Human weakMan = new StrongMan(man);
-        weakMan.run();
+        //装饰后的对象
+        Human strongMan = new StrongMan(man);
+        strongMan.run();
+        System.out.println("--------------------");
+        //装饰后的对象再次装饰
+        Human strongFlyMan = new StrongMan(flyMan);
+        strongFlyMan.run();
+
+
     }
 
 }
